ef5d723a53e7770b925912cbd5c834d78136a597
DioDiZy <104825699+DioDiZy@users.noreply.github.com>
2024-12-09 06:07:28 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "kasihcinta";
+$host = "btd2ishhks9bqlvbicyt-mysql.services.clever-cloud.com";
+$user = "uzx5rpy9fd8ngm0d";
+$pass = "FXd0uin6iaOl6WgAehaq";
+$db = "btd2ishhks9bqlvbicyt";
 
 if (!$con = mysqli_connect($host, $user, $pass, $db)) {
 
